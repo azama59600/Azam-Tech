fbf53a2d47b9515b5d16dfdd601f19d7228fbeb5
Adam Azam <azama59600@gmail.com>
2019-05-12 20:50:29 +0100

    track panels added

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function myFunction(){
     var userInputValue = document.getElementById('searchTxt').value;
-    console.log("User Input Value: " + userInputValue);    
+    console.log("User Input Value: " + userInputValue);  
+    
+    document.getElementById("comicCharacterTitle").innerHTML = userInputValue;
+
+    var dcCharacterSite = ("https://www.dccomics.com/characters/" + userInputValue)
+    
 };

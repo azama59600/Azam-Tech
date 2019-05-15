4b871f6df62868c8a8b89d1d48127d67b9ff9faf
Adam Azam <azama59600@gmail.com>
2019-05-15 20:22:47 +0100

    MARVEL iframe results shown

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 function myFunction(){
+    var x = document.getElementById("result-container");
+
+
     var userInputValue = document.getElementById('searchTxt').value;
     console.log("User Input Value: " + userInputValue);  
-    
-    document.getElementById("comicCharacterTitle").innerHTML = userInputValue;
+        
+    var marvelCharacterSite = ("https://www.marvel.com/characters/")
 
-    var dcCharacterSite = ("https://www.dccomics.com/characters/" + userInputValue)
-    
+    var userCharacterSite = ("https://www.marvel.com/search?limit=20&query=" + userInputValue + "&offset=0&content_type=characters")
+    var userComicsSite = ("https://www.comixology.co.uk/search?search=" + userInputValue)
+    var userMoviesSite = ("https://www.marvel.com/search?limit=20&query=" + userInputValue + "&offset=0&content_type=movies")
+    var userTvSite = ("https://www.marvel.com/search?limit=20&query=" + userInputValue + "&offset=0&content_type=tv")
+
+    document.getElementById('characterIframe').src = userCharacterSite;
+    document.getElementById('comicsIframe').src = userComicsSite;
+    document.getElementById('movieIframe').src = userMoviesSite;
+    document.getElementById('tvIframe').src = userTvSite;
+
+    console.log("Character Site: " + userCharacterSite);  
+    console.log("Comics Site: " + userComicsSite);  
+    console.log("Movie Site: " + userMoviesSite);  
+    console.log("TV series Site: " + userTvSite);  
 };

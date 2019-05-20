494e13db5bf0ec2080b4defe4c9959d4e7697065
Adam Azam <azama59600@gmail.com>
2019-05-20 21:12:41 +0100

    ADDED BUTTONS, CHANGED COLOUR SCHEME,  CHANGED LAYOUT

--- script.js
+++ script.js
@@ -5,9 +5,80 @@ function openManageDatabaseWindow(){
     }
 }
 
+function addGraphicNovelWindow(){
+  console.log("----HI-----")
+  var x = document.getElementById("add-entry-pop-up-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+function alterGraphicNovelWindow(){
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+function deleteGraphicNovelWindow(){
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+function sortGraphicNovelWindow(){
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+function hideGraphicNovelWindow(){
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+
+
+function addComicBookWindow(){
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+function alterComicBookWindow(){
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+function deleteComicBookWindow(){
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+function sortComicBookWindow(){
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+function hideComicBookWindow(){
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+
+//CLOSE WINDOWS
 function closeManageDatabaseWindow(){
-    var x = document.getElementById("manage-databases-pop-up-box");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    }
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  }
+}
+function closeAddWindow(){
+  var x = document.getElementById("manage-databases-pop-up-box");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  }
 }
